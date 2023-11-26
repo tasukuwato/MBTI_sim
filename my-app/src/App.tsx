55c9553e493bc2d0ed205f3361a155c767f4c72e
tasukuwato <221205191@ccmailg.meijo-u.ac.jp>
2023-11-26 17:25:44 +0900

    add: commentout

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //useStateフックを初期値:9で呼びだす
+  //ここでの<number>はTypeScriptのジェネリックを使用しており、数値型宣言である
   const [playerCount, setPlayerCount] = useState<number>(9);
 
   return (
@@ -13,11 +15,15 @@ function App() {
           Select the number of players for the game:
         </p>
 
-        <select
+
+        <select  
+        //selectタグでプルダウンを実装
           value={playerCount}
           onChange={(e) => setPlayerCount(parseInt(e.target.value))}
         >
+        
           {[9, 10, 11, 12, 13, 14, 15, 16].map(num => (
+            //プルダウンで選択可能な数字の候補
             <option key={num} value={num}>{num}</option>
           ))}
         </select>
