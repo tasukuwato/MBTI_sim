3593e6328f6ed804de59bc9af5cdb1f1f11c65eb
tasukuwato <221205191@ccmailg.meijo-u.ac.jp>
2023-11-27 17:33:27 +0900

    参加人数に応じてreactアイコンを表示する仕様に変更しました。

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,37 +5,47 @@ import './App.css';
 function App() {
   //useStateフックを初期値:9で呼びだす
   //ここでの<number>はTypeScriptのジェネリックを使用しており、数値型宣言である
-  const [playerCount, setPlayerCount] = useState<number>(9);
+  const [participantCount, setParticipantCount] = useState<number>(9);
+
+  const renderIcons = () => {
+    return Array.from({ length: participantCount }, (_, index) => (
+      <img key={index} src={logo} className="App-logo" alt="role-icon" style={{ width: '50px', height: '50px', margin: '5px'}} />
+    ));
+  };
+
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Select the number of players for the game:
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
 
-
-        <select  
-        //selectタグでプルダウンを実装
-          value={playerCount}
-          onChange={(e) => setPlayerCount(parseInt(e.target.value))}
-        >
-        
-          {[9, 10, 11, 12, 13, 14, 15, 16].map(num => (
-            //プルダウンで選択可能な数字の候補
-            <option key={num} value={num}>{num}</option>
-          ))}
-        </select>
-        <p>Number of Players: {playerCount}</p>
-        <a
+        <div>
+          <label htmlFor="participant-count">参加人数を選択：</label>
+          <select
+            id="participant-count"
+            value={participantCount}
+            onChange={(e) => setParticipantCount(parseInt(e.target.value))}
+          >
+            {[...Array(8)].map((_, i) => (
+              <option key={i + 9} value={i + 9}>{i + 9}</option>
+            ))}
+            </select>
+        </div>
+        {/* アイコンの表示 */}
+        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', marginTop: '20px' }}>
+          {renderIcons()}
+        </div>
+        <a 
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a>  
       </header>
     </div>
   );
